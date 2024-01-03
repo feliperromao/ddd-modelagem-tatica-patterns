6dab2d1fe0a857a6d1082609630f5bd674784fbd
feliperromao <feliperomaocad@gmail.com>
2024-01-03 00:18:17 -0300

    chore: first commit

---
+++ src/entity/customer.ts
@@ -0,0 +1,44 @@
+import Address from "./address";
+
+class Customer {
+    _id: string;
+    _name: string;
+    _address!: Address;
+    _active: boolean = false;
+
+    constructor(id: string, name: string) {
+        this._id = id;
+        this._name = name;
+        this.validate();
+    }
+
+    validate() {
+        if (this._id.length === 0) {
+            throw new Error("Id is required");
+        }
+
+        if (this._name.length === 0) {
+            throw new Error("Name is required");
+        }
+    }
+
+    changeName(name: string) {
+        this._name = name;
+        this.validate();
+    }
+
+    activate() {
+        if (this._address === undefined) {
+            throw new Error("Address is required to activade a customer")
+        }
+        this._active = true;
+    }
+
+    deactivate() {
+        this._active = false;
+    }
+
+    setAddress(address: Address) {
+        this._address = address;
+    }
+}
